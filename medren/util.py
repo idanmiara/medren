599935f2a1f5b4d2585c52d2872ba821286bafe2
Idan Miara <idan@miara.com>
2025-05-05 10:02:58 +0300

    .

--- medren/util.py
+++ medren/util.py
@@ -1,3 +1,5 @@
-def filename_safe(s: str) -> str:
-    chars_to_keep = (' ','.','_')
-    return "".join(c for c in s if c.isalnum() or c in chars_to_keep).rstrip()
+import re
+
+
+def filename_safe(dirty: str) -> str:
+    return re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "-", dirty)
